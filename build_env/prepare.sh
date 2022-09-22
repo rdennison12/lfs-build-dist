ef2153c729807ae0d9ec404a1d2201b55cf099dd
Rick Dennison <rdennison12@gmail.com>
2022-09-22 10:33:20 -0400

    Laptop rebuilt begin project over

--- build_env/prepare.sh
+++ build_env/prepare.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# For this project this script will check to see if LFS is set,
-# and set it if it's not.
-if [ -z "${LFS}" ]; then
-    export LFS=/home/rick/LFS/lfs-build-dist/build_env/build_root/
-else
-    echo "LFS is set ${LFS:?}"
-fi
+echo "LFS ${LFS}"
+export LFS=/home/rick/LFS-Build/lfs-build-dist/build_env/build_root
+echo "LFS is set ${LFS:?}"
